2aba8a3919ab254fceb0f94a577703df70fb7a11
A. Gordon <gordon@cshl.edu>
2009-08-10 16:08:39 -0400

    Barcode-Splitter: uses Galaxy's output.files_path to store multiple output files.
    No need for a web public folder and URL tweaking anymore.

--- galaxy/tools/fastx_toolkit/fastx_barcode_splitter_galaxy_wrapper.sh
+++ galaxy/tools/fastx_toolkit/fastx_barcode_splitter_galaxy_wrapper.sh
@@ -19,24 +19,21 @@
 #
 #This is a shell script wrapper for 'fastx_barcode_splitter.pl'
 #
-# 1. Output files are saved at a predefined location
-#    (Which was made publicly accessible using apache)
-#
+# 1. Output files are saved at the dataset's files_path directory.
+#    
 # 2. 'fastx_barcode_splitter.pl' outputs a textual table.
 #    This script turns it into pretty HTML with working URL
-#    (so lazy users can just click on the URLs and get thier files)
-
-BASEPATH="/media/sdb1/galaxy/barcode_splits/"
-PUBLICURL="http://tango.cshl.edu/barcode_splits/"
+#    (so lazy users can just click on the URLs and get their files)
 
 BARCODE_FILE="$1"
 FASTQ_FILE="$2"
 LIBNAME="$3"
-shift 3
+OUTPUT_PATH="$4"
+shift 4
 # The rest of the parameters are passed to the split program
 
-if [ "$LIBNAME" == "" ]; then
-	echo "Usage: $0 [BARCODE FILE] [FASTQ FILE] [LIBRARY_NAME]" >&2
+if [ "$OUTPUT_PATH" == "" ]; then
+	echo "Usage: $0 [BARCODE FILE] [FASTQ FILE] [LIBRARY_NAME] [OUTPUT_PATH]" >&2
 	exit 1
 fi
 
@@ -53,8 +50,16 @@ if [ ! -r "$BARCODE_FILE" ]; then
 	echo "Error: barcode file ($BARCODE_FILE) not found!" >&2
 	exit 1
 fi
+mkdir -p "$OUTPUT_PATH"
+if [ ! -d "$OUTPUT_PATH" ]; then
+	echo "Error: failed to create output path '$OUTPUT_PATH'" >&2
+	exit 1
+fi
 
-PREFIX="$BASEPATH"`date "+%Y-%m-%d_%H%M__"`"${LIBNAME}__"
+PUBLICURL=""
+BASEPATH="$OUTPUT_PATH/"
+#PREFIX="$BASEPATH"`date "+%Y-%m-%d_%H%M__"`"${LIBNAME}__"
+PREFIX="$BASEPATH""${LIBNAME}__"
 SUFFIX=".txt"
 
 RESULTS=`zcat -f "$FASTQ_FILE" | fastx_barcode_splitter.pl --bcfile "$BARCODE_FILE" --prefix "$PREFIX" --suffix "$SUFFIX" "$@"`
@@ -66,11 +71,10 @@ fi
 # Convert the textual tab-separated table into simple HTML table,
 # with the local path replaces with a valid URL
 echo "<html><body><table border=1>"
-echo "$RESULTS" | sed "s|$BASEPATH|$PUBLICURL|" | sed '
+echo "$RESULTS" | sed -r "s|$BASEPATH(.*)|<a href=\"\\1\">\\1</a>|" | sed '
 i<tr><td>
 s|\t|</td><td>|g
-s|http.*|<a href="&">&<\/a>|
 a<\/td><\/tr>
 '
-echo "<p><b>Copy these files to your local computer, as they will be soon deleted.</b>"
+echo "<p>"
 echo "</table></body></html>"
